72311692b293369b9453474d3e188a00579cf7a2
genevauxs <stefangenevaux@gmail.com>
2026-01-25 19:37:05 +0100

    Nodes Finish

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
     TEXT = "text"
@@ -26,3 +27,21 @@ class TextNode:
 
     def __repr__(self) -> str:
         return f"TextNode({self.text!r}, {self.text_type.value!r}, {self.url!r})"
+
+def text_node_to_html_node(text_node: TextNode):
+    if text_node.text_type.value not in TextType:
+        raise Exception(f"{text_node.text_type.value} is not a valid TextType.")
+    match text_node.text_type.value:
+        case "text":
+            return LeafNode(None, text_node.text)
+        case "bold":
+            return LeafNode("b", text_node.text)
+        case "italic":
+            return LeafNode("i", text_node.text)
+        case "code":
+            return LeafNode("code", text_node.text)
+        case "link":
+            return LeafNode("a", text_node.text, {"href":text_node.url})
+        case "image":
+            return LeafNode("img", "", {"src":text_node.url,
+                                        "alt": text_node.text})
