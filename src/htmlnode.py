1507007bf85ad3721dca1028d16d24d01b7377aa
genevauxs <stefangenevaux@gmail.com>
2026-01-25 19:20:44 +0100

    Leaf and Parent Node implementation

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -19,3 +19,33 @@ class HTMLNode:
     def __repr__(self):
         return f"HTMLNode({self.tag!r}, {self.value!r}, {self.children!r}, {self.props!r})"
     
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value , props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("LeafNode must have a value to convert to HTML")
+        if self.tag is None:
+            return f"{self.value}"
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag!r}, {self.value!r}, {self.props!r})"
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Parent Node has no tag value.")
+        if self.children is None:
+            raise ValueError("Parent Node has no children.")
+        res = f"<{self.tag}{self.props_to_html()}>"
+        for child in self.children:
+            res = res + child.to_html()
+        res = res + f"</{self.tag}>"
+        return res
