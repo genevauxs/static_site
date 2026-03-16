92d29557fc0612ef3103d68247bc77a8e3170f24
genevauxs <stefangenevaux@gmail.com>
2026-03-16 12:59:09 +0100

    Implementing first block seperation logic

---
+++ src/test_blocks.py
@@ -0,0 +1,95 @@
+import unittest
+
+
+from blocknode import block_to_block_type, BlockType, markdown_to_blocks
+
+class TestHTMLNode(unittest.TestCase):
+    def test_markdown_to_blocks(self):
+        md = """
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+    """
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+                [
+                    "This is **bolded** paragraph",
+                    "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                    "- This is a list\n- with items",
+                ],
+            )
+        
+    def test_markdown_to_blocks_single_block(self):
+        md = """
+This is a single paragraph
+with no blank lines
+separating anything
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is a single paragraph\nwith no blank lines\nseparating anything",
+            ],
+        )
+
+    def test_markdown_to_blocks_strips_whitespace(self):
+        md = """
+   # Heading with spaces   
+
+   Some paragraph text   
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "# Heading with spaces",
+                "Some paragraph text",
+            ],
+        )
+    
+    def test_heading_levels(self):
+    # All 6 heading levels should be valid
+        for i in range(1, 7):
+            block = f"{'#' * i} heading"
+            self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+
+    def test_heading_too_many_hashes(self):
+        # 7 hashes is not a valid heading
+        block = "####### not a heading"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_heading_no_space(self):
+        # Hash without a space is not a heading
+        block = "#heading"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_ordered_list_wrong_start(self):
+        # Ordered list must start at 1
+        block = "2. item\n3. item"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_ordered_list_non_sequential(self):
+        # Numbers must increment by 1
+        block = "1. item\n3. item"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_unordered_list_missing_space(self):
+        # Dash without a trailing space is not a valid unordered list
+        block = "-item\n-item"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_mixed_block_falls_back_to_paragraph(self):
+        # A block that starts like a quote but has a non-quote line
+        block = "> quote line\nnot a quote line"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_code_single_line_not_valid(self):
+        # A single line of backticks doesn't count as a code block
+        block = "```code```"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -5,6 +5,7 @@ from functions import (
     split_nodes_link,
     extract_markdown_links,
     extract_markdown_images,
+    text_to_textnodes,
 )
 
 from textnode import TextNode, TextType
@@ -155,6 +156,35 @@ class TestInlineMarkdown(unittest.TestCase):
                 TextNode(" with text that follows", TextType.TEXT),
             ],
             new_nodes,
+        )
+
+    def test_text_to_textnodes(self):
+        nodes = text_to_textnodes(
+            "This is **text** with an _italic_ word and a `code block` and an ![image](https://i.imgur.com/zjjcJKZ.png) and a [link](https://boot.dev)"
+        )
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            nodes,
+        )
+
+    def test_text_to_textnodes_plain_text(self):
+        nodes = text_to_textnodes("Just some plain text with no formatting")
+        self.assertListEqual(
+            [
+                TextNode("Just some plain text with no formatting", TextType.TEXT),
+            ],
+            nodes,
         )
 
 if __name__ == "__main__":
